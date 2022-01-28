e15c23e8311fa4e95aae37f621d7dbe6ea472d17
Yossawadee Hoymala <h.yossawadee@gmail.com>
2022-01-28 10:16:23 +0700

    edit app.js

--- app.js
+++ app.js
@@ -1,10 +1,64 @@
+// const express = require('express')  // ใช้งาน module express
+// const app = express()  // สร้างตัวแปร app เป็น instance ของ express
+// const createError = require('http-errors') // เรียกใช้งาน http-errors module
+// const port = 3000  // port 
+  
+// // ส่วนของการใช้งาน router module ต่างๆ 
+// const userApi = require('./api/users')
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.set('view options', {delimiter: '?'});
+// // app.set('env','production')
+  
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
+// app.use(express.static(path.join(__dirname, 'public')))
+  
+// // เรียกใช้งาน indexRouter
+// app.use('/api', [userApi]) 
+ 
+// // ทำงานทุก request ที่เข้ามา 
+// app.use(function(req, res, next) {
+//     var err = createError(404)
+//     next(err)
+// })
+  
+// // ส่วนจัดการ error
+// app.use(function (err, req, res, next) {
+//     // กำหนด response local variables 
+//     res.locals.message = err.message
+//     res.locals.error = req.app.get('env') === 'development' ? err : {}
+  
+//     // กำหนด status และ render หน้า error page
+//     res.status(err.status || 500) // ถ้ามี status หรือถ้าไม่มีใช้เป็น 500
+//     res.render('error') 
+// })
+  
+// app.listen(port, function() {
+//     console.log(`Example app listening on port ${port}!`)
+// })
+
+
 const express = require('express')  // ใช้งาน module express
 const app = express()  // สร้างตัวแปร app เป็น instance ของ express
+const path = require('path') // เรียกใช้งาน path module
 const createError = require('http-errors') // เรียกใช้งาน http-errors module
 const port = 3000  // port 
   
 // ส่วนของการใช้งาน router module ต่างๆ 
 const userApi = require('./api/users')
+  
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('view options', {delimiter: '?'});
+// app.set('env','production')
+  
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
   
 // เรียกใช้งาน indexRouter
 app.use('/api', [userApi]) 
@@ -27,5 +81,5 @@ app.use(function (err, req, res, next) {
 })
   
 app.listen(port, function() {
-    console.log(`Example app listening on port ${port}!`)
+    console.log(`listening on port ${port}!`)
 })
