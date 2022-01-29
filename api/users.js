b1537cd4fd255a906cb771adc504481f17d25446
Yossawadee Hoymala <h.yossawadee@gmail.com>
2022-01-29 20:05:16 +0700

    get user by id

--- api/users.js
+++ api/users.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/config"); // เรียกใช้งานเชื่อมกับ MySQL
 
-router
-  .route("/users?")
+router.route("/users?")
   .get((req, res, next) => {
     let sql = " SELECT * FROM usercalendar ";
     db.query(sql, (error, results, fields) => {
@@ -11,7 +10,7 @@ router
       if (error)
         return res.status(500).json({
           status: 500,
-          message: "Internal Server Error",
+          message: "Internal Server Error", //error.sqlMessage
         });
 
       const result = {
@@ -24,10 +23,10 @@ router
 
   .post((req, res, next) => {
     let user = {
-      name: req.body.name,
+      user_id: req.body.userId,
+      name: req.body.displayName,
       username: req.body.username,
       email: req.body.email,
-      hashpassword: req.body.hashpassword,
       role_id: req.body.role_id,
     };
     let sql = "INSERT INTO usercalendar SET ?";
@@ -35,17 +34,43 @@ router
       if (error)
         return res.status(500).json({
           status: 500,
-          message: "Internal Server Error",
+          message: "Internal Server Error", //error.sqlMessage
         });
       user = [{ id: results.user_id, ...user }];
-      // let id = results.insertId
-      // console.log(id)
       const result = {
         status: 200,
+        message: "User was registered",
         data: user,
       };
       return res.json(result);
     });
   });
 
-module.exports = router;
+  router.route('/user/:user_id')
+    .all((req, res, next) => { 
+        let sql = ' SELECT * FROM usercalendar WHERE user_id = ? '
+        db.query(sql, [req.params.user_id], (error, results, fields)=>{
+            // เกิด error ในคำสั่ง sql
+            if(error) return res.status(500).json({
+                "status": 500,
+                "message": "Internal Server Error"
+            })
+            // กรณีไม่พบข้อมูล 
+            if(results.length === 0) return res.status(400).json({
+                "status": 400,
+                "message": "Not found user with the given ID"
+            }) 
+            res.user = results
+            next()  
+        })        
+    })
+    .get((req, res, next) => { 
+        const result = {
+            "status": 200,
+            "data": res.user
+        }
+        return res.json(result)
+    })
+
+
+module.exports = router
